07facaba0c14f2756f3bf5db2dba577b0f8d060a
Jatan Dudeja <dudeja.jatan04@gmail.com>
2024-01-29 17:55:13 +0530

    working

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://bloggers.onrender.com"],
+    origin: "http://localhost:3000",
     credentials: true,
   })
 );
